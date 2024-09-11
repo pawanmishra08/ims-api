527b187b039aade83510dadc8438dea7fad953e3
Pawan Mishra <pawanm9816@gmail.com>
2024-09-11 15:27:51 +0545

    completed users crud API

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
@@ -6,6 +6,7 @@ import { captalizeFirstLetterOfEachWordInPhrase } from 'src/helpers/captialize';
 import { RolesService } from 'src/roles/roles.service';
 import { OrganizationsService } from 'src/organizations/organizations.service';
 import { hash } from 'bcrypt';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
@@ -33,19 +34,48 @@ export class UsersService {
   }
 
   findAll() {
-    return `This action returns all users`;
+    return this.prismaService.user.findMany;
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} user`;
+    return this.getuserById(id);
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+   async update(id: number, updateUserDto: UpdateUserDto) {
+    await this.getuserById(id);
+
+    const roleservice = new RolesService(this.prismaService)
+    const organizationservice = new OrganizationsService(this.prismaService);
+
+    await roleservice.findOne(updateUserDto.role_id)
+    await organizationservice.findOne(updateUserDto.organization_id);
+
+
+    if(updateUserDto.name) {
+      updateUserDto.name= captalizeFirstLetterOfEachWordInPhrase(updateUserDto.name)
+    }
+
+    if (!await this.checkIfEmailExist(updateUserDto.email ,id)){
+      throw new BadRequestException("this email has been already taken")
+     }
+     
+     if (!await this.checkIfmobileExist(updateUserDto.mobile , id)){
+      throw new BadRequestException("this email has been already taken")
+     }
+      
+     if(updateUserDto.password){
+      updateUserDto.password = await hash(updateUserDto.password, 10)
+     }
+     return this.prismaService.user.update({
+      where: { id },
+      data: updateUserDto
+     });
+
   }
 
   remove(id: number) {
-    return `This action removes a #${id} user`;
+    return this.getuserById(id);
+    return this.prismaService.user.delete({ where: { id }})
   }
 
   private async checkIfEmailExist(email: string, id?:number): Promise<boolean> {
@@ -69,5 +99,15 @@ export class UsersService {
     }
 
     return !!user;
+  }
+
+  private async getuserById(id: number) {
+    const organization = await this.prismaService.user.findFirst({ where: { id } });
+
+    if (!User) {
+      throw new NotFoundException(`user with id ${id} does not exist`);
+    }
+
+    return organization;
   }
 }
