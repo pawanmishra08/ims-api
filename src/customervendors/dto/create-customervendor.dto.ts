6d22472bd39f01e2a1fe1f08a756af2f9b033945
Pawan Mishra <pawanm9816@gmail.com>
2024-12-25 20:49:17 +0545

    customer vendor dto added

--- src/customervendors/dto/create-customervendor.dto.ts
+++ src/customervendors/dto/create-customervendor.dto.ts
@@ -1,1 +1,40 @@
-export class CreateCustomervendorDto {}
+import { IsBoolean, IsNumber, IsOptional, isString, IsString, MaxLength } from "class-validator";
+
+export class CreateCustomervendorDto {
+
+    @IsString()
+    name: string;
+
+    @IsString()
+    @IsOptional()
+    Description?: string;
+
+    @IsString()
+    @IsOptional()
+    email: string;
+
+    @IsNumber()
+    @IsOptional()
+    @MaxLength(15)
+    phone: number;
+
+    @IsString()
+    @IsOptional()
+    street?: string;
+
+    @IsString()
+    @IsOptional()
+    city?: string;
+
+    @IsString()
+    @IsOptional()
+    district?: string;
+
+    @IsString()
+    @IsOptional()
+    province?: string;
+
+    @IsString()
+    @IsOptional()
+    is_vendor?: boolean= false;
+}
